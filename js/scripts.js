e4d1f1181eaf91e7ea4185ecd8c4dd454fbd05ba
Henry <henrymoire@gmail.com>
2020-04-14 22:47:34 +0300

    Center aligned social section and add name and year to end of webpage

--- js/scripts.js
+++ js/scripts.js
@@ -19,15 +19,3 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    $("#blanks form").submit(function() {
-        $(".person1").append("blah blah");
-        $(".person2").append("blah blah");
-        $(".animal").append("blah blah");
-        $(".exclamation").append("blah blah");
-        $(".verb").append("blah blah");
-        $(".noun").append("blah blah");
-
-        $("#story").show();
-    });
-});
