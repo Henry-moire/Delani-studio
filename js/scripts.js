cb33c6829d18b50056dc8c6b42fc5b9aa2577327
Henry <henrymoire@gmail.com>
2020-04-14 21:08:03 +0300

    Add clickability and text to what we do section

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".designClickable").click(function() {
+      $("#designShowing").toggle();
+      $("#designHidden").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".developmentClickable").click(function() {
+      $("#developmentShowing").toggle();
+      $("#developmentHidden").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".productClickable").click(function() {
+      $("#productShowing").toggle();
+      $("#productHidden").toggle();
+    });
+});
